fa9dc68a5eadb24b0cfc9b006211e227428fc61d
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-23 20:28:35 +0700

    error cannot read tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  const increment = () => {
+    setCount(count+1)
+  }
+
+  const decrement = () => {
+    setCount(count-1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Hello, I am <strong>Pandhu Arya<span style={{color: 'blue'}}>.</span></strong>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto mt-8 text-center">
+    <h1 className="text-4xl font-bold mb-4">Counter App</h1>
+    <p className="text-2xl mb-4">Count: {count}</p>
+    <div className="flex justify-center space-x-4">
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={increment}
+      >
+        Increment
+      </button>
+      <button
+        className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+        onClick={decrement}
+      >
+        Decrement
+      </button>
     </div>
+  </div>
   );
 }
 
