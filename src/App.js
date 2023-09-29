7fba6e8e1134d39bccc1330d842a9b5fd9d67d0d
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-29 13:13:43 +0700

    create component counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import Counter from "./component/Counter"
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
   return (
-    <div className="container unselectable">
-      <h1>Counter App</h1>
-      <p className="counter">Count: {count}</p>
-      <div className="container-btn">
-        <button onClick={decrement} className="btn red">Decrement</button>
-        <button onClick={increment} className="btn green">Increment</button>
-      </div>
-    </div>
+    <Counter/>
   );
 }
 
---
+++ src/component/Counter.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+  
+    const increment = () => {
+      setCount(count + 1);
+    };
+  
+    const decrement = () => {
+      setCount(count - 1);
+    };
+  
+    return (
+      <div className="container unselectable">
+        <h1>Counter App</h1>
+        <p className="counter">Count: {count}</p>
+        <div className="container-btn">
+          <button onClick={decrement} className="btn red">Decrement</button>
+          <button onClick={increment} className="btn green">Increment</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Counter;
